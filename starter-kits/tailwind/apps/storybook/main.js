1db1352eb9310d46565a18e7d7afd4b92bab1d64
Christian Wiedemann <christian.wiedemann@key-tec.de>
2024-12-11 14:32:55 +0100

    fix: Add storybook stylelint

--- starter-kits/tailwind/apps/storybook/main.js
+++ starter-kits/tailwind/apps/storybook/main.js
@@ -13,6 +13,29 @@ export default {
     '@storybook/addon-essentials',
     {
       name: '@storybook/addon-styling-webpack',
-    },
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  implementation: require.resolve('postcss'),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
   ],
 };
